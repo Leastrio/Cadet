93a4c46185a652137493e96fe49f610d175c3e8e
Leastrio <jacob@leastr.io>
2023-02-10 19:04:18 -0700

    update mix file and publish

--- mix.exs
+++ mix.exs
@@ -7,8 +7,34 @@ defmodule Cadet.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      name: "Cadet",
+      source_url: "https://github.com/Leastrio/Cadet",
+      package: package(),
+      docs: docs()
     ]
+  end
+
+  defp docs do
+    [
+      main: "Cadet"
+    ]
+  end
+
+  defp package do
+    [
+      name: :cadet,
+      licenses: ["MIT"],
+      maintainers: ["Leastrio"],
+      links: %{
+        "Github" => "https://github.com/Leastrio/Cadet"
+      }
+    ]
+  end
+
+  defp description do
+    "A command handler for the Nostrum discord API wrapper"
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -21,6 +47,7 @@ defmodule Cadet.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
     ]
   end
 end
